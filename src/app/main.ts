eca8f1a5d0355d2179014333ee2304af3c99fc23
vivekcsein <vivekcse.in@gmail.com>
2026-06-30 01:35:02 +0530

    vercel compatible to export app by defualt

--- src/app/main.ts
+++ src/app/main.ts
@@ -3,38 +3,34 @@ import { Hono } from "hono";
 import { registerMiddlewares } from "@/packages/bootstrap/middlewares.bootstrap";
 import { registerRoutes } from "@/packages/bootstrap/routes.bootstrap";
 
-const createApp = (): Hono => {
-	const app = new Hono();
+const app = new Hono();
 
-	// Register application middleware
-	registerMiddlewares(app);
+// Register global middleware
+registerMiddlewares(app);
 
-	// Register application routes
-	registerRoutes(app);
+// Register application routes
+registerRoutes(app);
 
-	// Global error handler
-	app.onError((error, c) => {
-		return c.json(
-			{
-				success: false,
-				message: error.message,
-			},
-			500,
-		);
-	});
+// Global error handler
+app.onError((error, c) => {
+	return c.json(
+		{
+			success: false,
+			message: error.message,
+		},
+		500,
+	);
+});
 
-	// Not found handler
-	app.notFound((c) => {
-		return c.json(
-			{
-				success: false,
-				message: `Route "${c.req.path}" not found.`,
-			},
-			404,
-		);
-	});
+// Not Found handler
+app.notFound((c) => {
+	return c.json(
+		{
+			success: false,
+			message: `Route "${c.req.path}" not found.`,
+		},
+		404,
+	);
+});
 
-	return app;
-};
-
-export default createApp;
+export default app;
